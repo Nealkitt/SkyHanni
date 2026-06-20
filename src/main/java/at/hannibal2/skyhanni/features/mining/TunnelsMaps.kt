d4bc1066d3f23d8627a16fc87aa4d8a9c835b4b3
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-06-20 10:50:23 +0200

    untesting repo patterns and changing wrong link in repo docs

--- src/main/java/at/hannibal2/skyhanni/features/mining/TunnelsMaps.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/TunnelsMaps.kt
@@ -144,10 +144,6 @@ object TunnelsMaps {
         "mining.commisson.completed",
         "§a§lCOMPLETED",
     )
-    private val completedPatternTest by RepoPattern.pattern(
-        "mining.commisson.completed.test",
-        "This is a §cbloody §ftest",
-    )
     private val commissionInvPattern by RepoPattern.pattern(
         "mining.commission.inventory",
         "Commissions",
